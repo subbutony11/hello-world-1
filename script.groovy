3e07432b059d691e8800c2e2ba4708153a36468d
subbutony11 <105411669+subbutony11@users.noreply.github.com>
2022-07-05 17:58:49 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,15 @@
-def buildapp() {
+def buildjar() {
   echo "building the application..."
+  sh 'mvn package'
 }
-def testapp() {
-  echo "testing the application..."
+def buildimage() {
+  echo "building the docker image..."
+   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                       sh 'docker build -t subhashinikuruva/my-repo:redis-2.0 .'
+                       sh "echo $PASS | docker login -u $USER --password-stdin"
+                       sh 'docker push subhashinikuruva/my-repo:redis-2.0'
 }
 def deployapp() {
   echo "deploying the application..."
-  echo "deploying version ${params.VERSION}"
 } 
 return this
