c8b1d080c48bd741df09752feacc39c438092d73
subbutony11 <105411669+subbutony11@users.noreply.github.com>
2022-07-07 14:20:56 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -15,8 +15,8 @@ def deployapp() {
   docker.withRegistry(
             'https://docker push 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:latest',
             'ecr:ap-south-1:aws-credentials') {
-            redis-1.1 = docker.build('subhashinikuruva/my-repo')
-             redis-1.1.push('latest')
+            myImage = docker.build('subhashinikuruva/my-repo')
+             myImage.push('latest')
           }
   
 } 
