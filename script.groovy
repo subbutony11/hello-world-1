3c804f4ee7844d9ebfeaf4810bff2146a85d6f63
subbutony11 <105411669+subbutony11@users.noreply.github.com>
2022-07-08 18:45:17 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,7 +13,7 @@ def buildimage() {
 def deployapp() {
   echo "deploying the application.."
   withEnv (["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}","AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}", "AWS_DEFAULT_REGION=${env.AWS_DEFAULT_REGION}"]){
-            sh 'aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 222882145299.dkr.ecr.ap-south-1.amazonaws.com'
+            sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin  '
             sh 'docker build -t jenkins-pipeline-build .'
             sh 'docker tag jenkins-pipeline-build:latest 222882145299.dkr.ecr.ap-south-1.amazonaws.com/jenkins-pipeline-build:latest'
             sh 'docker push 222882145299.dkr.ecr.ap-south-1.amazonaws.com/jenkins-pipeline-build:latest'
