1b18d73dd42f48ca8ab337af2f028ecc0e800eb0
subbutony11 <105411669+subbutony11@users.noreply.github.com>
2022-07-08 16:06:48 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,7 +13,7 @@ def buildimage() {
 def deployapp() {
   echo "deploying the application.."
   withEnv (["AWS_ACCESS_KEY_ID=${env.AWS_ACCESS_KEY_ID}","AWS_SECRET_ACCESS_KEY=${env.AWS_SECRET_ACCESS_KEY}", "AWS_DEFAULT_REGION=${env.AWS_DEFAULT_REGION}"]){
-            sh 'aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 222882145299.dkr.ecr.ap-south-1.amazonaws.com'
+            sh 'docker login -u AWS -p '
             sh 'docker build -t redis .'
             sh 'docker tag redis:latest 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:latest:""BUILD_ID""'
             sh 'docker push 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:latest:""BUILD_ID""'
