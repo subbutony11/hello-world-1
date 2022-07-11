c83fbac417c67ab2f750b44d7d4e4605ef54553d
subbutony11 <105411669+subbutony11@users.noreply.github.com>
2022-07-11 21:09:26 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,8 +16,8 @@ def deployapp() {
             sh 'echo docker login -u  AWS -p  $(aws ecr get-login-password --region ap-south-1)  222882145299.dkr.ecr.ap-south-1.amazonaws.com'
             sh 'eval $(aws ecr get-login --no-include-email --region ap-south-1 | describe-registry)'
             sh 'docker build -t redis .'
-            sh 'docker tag redis:latest 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:latest'
-            sh 'docker push 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:latest'
+            sh 'docker tag redis:latest 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:1.1'
+            sh 'docker push 222882145299.dkr.ecr.ap-south-1.amazonaws.com/redis:1.1'
             }
   
 }
